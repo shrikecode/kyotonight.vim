7ebc92a52aaa953e87ec1511e81047c26625b5d2
Wojciech M. Wnuk <laniusone@pm.me>
2023-03-21 13:31:54 +0100

    Fix tym theme

--- extras/tym/theme.lua
+++ extras/tym/theme.lua
@@ -1,7 +1,7 @@
 -- name: Kyoto Night
 -- license: MIT
 -- author: Wojciech M. Wnuk <laniusone@pm.me>
--- upstream: https://github.com/laniusone/kyotonight.vim/raw/master/extras/tym/theme.lua
+-- upstream: https://github.com/voidekh/kyotonight.vim/raw/master/extras/tym/theme.lua
 
 local bg = '#1a1b26'
 local fg = '#a9b1d6'
@@ -14,20 +14,20 @@ return {
     color_cursor_foreground = bg,
     color_highlight = '#33467c',
     color_highlight_foreground = '#c0caf5',
-    color0  = '#32344a',
-    color1  = '#f7768e',
-    color2  = '#9ece6a',
-    color3  = '#e0af68',
-    color4  = '#7aa2f7',
-    color5  = '#ad8ee6',
-    color6  = '#73daca',
-    color7  = '#787c99',
-    color8  = '#444b6a',
-    color9  = '#ff7a93',
-    color10 = '#b9f27c',
-    color11 = '#ff9e64',
-    color12 = '#7da6ff',
-    color13 = '#bb9af7',
-    color14 = '#0db9d7',
-    color15 = '#acb0d0',
+    color_0  = bg,
+    color_1  = '#f7768e',
+    color_2  = '#9ece6a',
+    color_3  = '#e0af68',
+    color_4  = '#7aa2f7',
+    color_5  = '#ad8ee6',
+    color_6  = '#73daca',
+    color_7  = '#787c99',
+    color_8  = '#444b6a',
+    color_9  = '#ff7a93',
+    color_10 = '#b9f27c',
+    color_11 = '#ff9e64',
+    color_12 = '#7da6ff',
+    color_13 = '#bb9af7',
+    color_14 = '#0db9d7',
+    color_15 = '#acb0d0',
 }
